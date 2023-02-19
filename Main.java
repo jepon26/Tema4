6f0e1e01de2919593fcda38a7eecac5c6b9258b7
Elvin Javier Rueda <elvinrueda@yahoo.es>
2023-02-18 21:30:41 -0600

    First commit

---
+++ Main.java
@@ -0,0 +1,60 @@
+public class Main {
+    public static void main(String[] args){
+         // if, else if, else
+        int numeroIf = 0;
+
+        if(numeroIf < 0){
+            System.out.println("The variable numeroIf" + numeroIf + "it is negative");
+        } else if (numeroIf > 0) {
+            System.out.println("The variable numeroIf" + numeroIf + "It is positive");
+        } else {
+            System.out.println("The variable numeroIf It is 0");
+        }
+    }
+}
+
+// while
+    int numberWhile = 1;
+
+ while(numberwhile < 3) {
+     numberWhile++;
+     System.out.println("The new value of variable numberWhile is: " + numberWhile);
+        }
+
+ //doWhile
+
+int numberdoWhile = 3,
+
+do {
+     numberdoWhile++;
+     System.out.println("The new value of variable numberdiWhile is: " + numberdoWhile );
+        } while(numbedoWhile < 3);
+
+//for
+
+for(int numberFor =0; numberFor <= 3; numberFor){
+    System.out.println("The new value of variable numberFor is: " + numberFor )
+        }
+
+//switch
+
+String season = "spring";
+     switch(season) {
+         case "spring":
+             System.out.println("It is spring");
+             break;
+         case "summer":
+             System.out.println("It is summer");
+             break;
+         case "fall":
+              System.out.println("It is fall");
+              break;
+         case "winter":
+             System.out.println("It is winte");
+             break;
+             default:
+                 System.out.println("It is not a season");
+        }
+      }
+
+}
